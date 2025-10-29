ef3b85831382a661557d10bdc46bc04af430388b
Илья Анушко <ilaanushko0@gmail.com>
2025-10-29 19:56:29 +0300

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,26 @@
-module.exports = {
-  root: true,
-  languageOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-  },
-  plugins: {
-    react: require('eslint-plugin-react'),
-    'react-native': require('eslint-plugin-react-native'),
-  },
-  linterOptions: {
-    reportUnusedDisableDirectives: 'error',
-  },
-  rules: {
-    'no-unused-vars': 'warn',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-  },
-  overrides: [
-    {
-      files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-      rules: {},
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactNative from 'eslint-plugin-react-native';
+
+export default [
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
     },
-  ],
-};
+    plugins: {
+      react: eslintPluginReact,
+      'react-native': eslintPluginReactNative,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
